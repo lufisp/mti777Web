f96a2c3f6236e6ed52824a2680b8260862f6b2b3
fernando <lufisp@gmail.com>
2016-01-31 21:52:18 -0500

    Comit 2

--- public_html/js/indexJs.js
+++ public_html/js/indexJs.js
@@ -3,11 +3,16 @@
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
  */
+
+var divOrderTable_html;
 $(document).ready(function () {
     loadTableRoom();
     $("#divOrder").hide();
     $("#divItemsOrder").hide();
     $("#pgto").hide();
+    
+    //keep the inside table html, to keep the headers and don't lose the style
+    divOrderTable_html = $("#divOrder_table").html();
 
 });
 
@@ -41,29 +46,26 @@ function loadTableRoom() {
 }
 
 function tableRoomClick(id) {
+    $("#divOrder").show();
+    
     jQuery.ajax({
         type: 'GET',
-        url: 'http://localhost:8080/table/',
+        url: 'http://localhost:8080/orderClient/table/' + id,
         dataType: 'json',
         success: function (data, textStatus, jqXHR) {
             var html = "";
             for (var tableIndex in data) {
-                html = html + "<tr onclick=tableRoomClick(" + data[tableIndex].idtable + ")>"
+                html = html + "<tr>"
                 html = html + " <td>"
-                html = html + data[tableIndex].idtable;
+                html = html + data[tableIndex].idOrder;
                 html = html + " </td>"
                 html = html + " <td>"
-                html = html + data[tableIndex].name;
+                html = html + data[tableIndex].openFlag;
                 html = html + " </td>"
-                html = html + " <td>"
-                html = html + data[tableIndex].availableFlag;
-                html = html + " </td>"
-                html = html + " <td>"
-                html = html + data[tableIndex].places;
-                html = html + " </td>"
-                html = html + "</tr>"
+                
             }
-            $("#divTableRoom_table").append(html);
+            //recover the html salved in the document load.
+            $("#divOrder_table").html(divOrderTable_html).append(html);
         }
     });
     console.log(id);
