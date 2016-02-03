1d93cbccd5fb3c84a7e493724a0e27deae9d322d
fernando <lufisp@gmail.com>
2016-02-03 13:02:15 -0500

    Comit 6

--- public_html/js/indexJs.js
+++ public_html/js/indexJs.js
@@ -7,12 +7,13 @@
 var divOrderTable_html;
 $(document).ready(function () {
     loadTableRoom();
-    $("#divOrder").hide();
-    $("#divItemsOrder").hide();
-    $("#pgto").hide();
+    $("#divOrder").css('visibility','hidden');
+    $("#divItemsOrder").css('visibility','hidden');
+    $("#pgto").css('visibility','hidden');
     
     //keep the inside table html, to keep the headers and don't lose the style
     divOrderTable_html = $("#divOrder_table").html();
+    divItemsTable_html = $("#divItems_table").html();
 
 });
 
@@ -46,7 +47,7 @@ function loadTableRoom() {
 }
 
 function tableRoomClick(id) {
-    $("#divOrder").show();
+    $("#divOrder").css('visibility','visible');
     
     jQuery.ajax({
         type: 'GET',
@@ -55,17 +56,54 @@ function tableRoomClick(id) {
         success: function (data, textStatus, jqXHR) {
             var html = "";
             for (var tableIndex in data) {
-                html = html + "<tr>"
+                html = html + "<tr onclick=orderClick(" + data[tableIndex].idOrder + ")>"
                 html = html + " <td>"
                 html = html + data[tableIndex].idOrder;
                 html = html + " </td>"
                 html = html + " <td>"
                 html = html + data[tableIndex].openFlag;
                 html = html + " </td>"
+                html = html + " </tr>"
+                
                 
             }
             //recover the html salved in the document load.
             $("#divOrder_table").html(divOrderTable_html).append(html);
+            $("#divItemsOrder").css('visibility','hidden');
+        }
+    });
+}
+
+function orderClick(id) {
+    $("#divItemsOrder").css('visibility','visible');
+    jQuery.ajax({
+        type: 'GET',
+        url: 'http://localhost:8080/orderItems/orderClient/' + id,
+        dataType: 'json',
+        success: function (data, textStatus, jqXHR) {
+            var html = "";
+            for (var tableIndex in data) {
+                html = html + "<tr>"
+                html = html + " <td>"
+                html = html + data[tableIndex].idorderItems;
+                html = html + " </td>"
+                html = html + " <td>"
+                html = html + data[tableIndex].itemMenuDet.version;
+                html = html + " </td>"
+                html = html + " <td>"
+                html = html + data[tableIndex].itemMenuDet.prix;
+                html = html + " </td>"
+                html = html + " <td>"
+                html = html + data[tableIndex].quantity;
+                html = html + " </td>"
+                html = html + " <td>"
+                html = html + data[tableIndex].quantity * data[tableIndex].itemMenuDet.prix;
+                html = html + " </td>"
+                html = html + "</tr>"
+                
+            }
+            //recover the html salved in the document load.
+            $("#divItems_table").html(divItemsTable_html).append(html);
         }
     });
     console.log(id);
