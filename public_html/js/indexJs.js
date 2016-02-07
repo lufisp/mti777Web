d7b52284cf3d14462ed0f0d85797e5444e951d1b
fernando <lufisp@gmail.com>
2016-02-07 12:24:21 -0500

    Comit 7

--- public_html/js/indexJs.js
+++ public_html/js/indexJs.js
@@ -4,15 +4,17 @@
  * and open the template in the editor.
  */
 
+var shiftId = 2;
+
 var divOrderTable_html;
 $(document).ready(function () {
     loadTableRoom();
-    $("#divOrder").css('visibility','hidden');
-    $("#divItemsOrder").css('visibility','hidden');
-    $("#pgto").css('visibility','hidden');
-    
+    $("#divOrder").css('visibility', 'hidden');
+    $("#divItemsOrder").css('visibility', 'hidden');
+    $("#pgto").css('visibility', 'hidden');
+
     //keep the inside table html, to keep the headers and don't lose the style
-    divOrderTable_html = $("#divOrder_table").html();
+    divOrder_html = $("#divOrder").html();
     divItemsTable_html = $("#divItems_table").html();
 
 });
@@ -26,7 +28,7 @@ function loadTableRoom() {
         success: function (data, textStatus, jqXHR) {
             var html = "";
             for (var tableIndex in data) {
-                html = html + "<tr onclick=tableRoomClick(" + data[tableIndex].idtable + ")>"
+                html = html + "<tr onclick=tableRoomClick(" + JSON.stringify(data[tableIndex]) + ")>"
                 html = html + " <td>"
                 html = html + data[tableIndex].idtable;
                 html = html + " </td>"
@@ -46,9 +48,14 @@ function loadTableRoom() {
     });
 }
 
-function tableRoomClick(id) {
-    $("#divOrder").css('visibility','visible');
-    
+function tableRoomClick(tableRoom) {
+    id = tableRoom.idtable;
+    tableName = tableRoom.name;
+    $("#divOrder").html(divOrder_html);
+    $("#divOrder").css('visibility', 'visible');
+    $("#divItemsOrder").css('visibility', 'hidden');
+    $("#mti777_orderLabelId").html("Orders  - " + tableName);
+
     jQuery.ajax({
         type: 'GET',
         url: 'http://localhost:8080/orderClient/table/' + id,
@@ -56,26 +63,34 @@ function tableRoomClick(id) {
         success: function (data, textStatus, jqXHR) {
             var html = "";
             for (var tableIndex in data) {
-                html = html + "<tr onclick=orderClick(" + data[tableIndex].idOrder + ")>"
-                html = html + " <td>"
-                html = html + data[tableIndex].idOrder;
-                html = html + " </td>"
-                html = html + " <td>"
-                html = html + data[tableIndex].openFlag;
-                html = html + " </td>"
-                html = html + " </tr>"
-                
-                
+                html += "<tr>";
+                html += " <td>";
+                html += data[tableIndex].idOrder;
+                html += " </td>";
+                html += " <td>";
+                html += data[tableIndex].openFlag;
+                html += " </td>";
+                html += " <td>";                
+                html += "<span onclick=orderClick(" + data[tableIndex].idOrder + ") class='glyphicon glyphicon-list-alt'></span>";                
+                html += "&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp";
+                html += "<span onclick=orderRemove(" + JSON.stringify(data[tableIndex]) + ") class='glyphicon glyphicon-remove'></span>";                
+                html += " </td>";
+                html += " </tr>";
             }
             //recover the html salved in the document load.
             $("#divOrder_table").html(divOrderTable_html).append(html);
-            $("#divItemsOrder").css('visibility','hidden');
+            htmlBtn = "<div>";
+            htmlBtn += "<button type='button' class='btn btn-default' onclick=addOrder(" + JSON.stringify(tableRoom) + ")>";
+            htmlBtn += "<span class='glyphicon glyphicon-plus'></span>";
+            htmlBtn += "Add";
+            htmlBtn += "</button></div>";
+            $("#divOrder").append(htmlBtn);
         }
     });
 }
 
 function orderClick(id) {
-    $("#divItemsOrder").css('visibility','visible');
+    $("#divItemsOrder").css('visibility', 'visible');
     jQuery.ajax({
         type: 'GET',
         url: 'http://localhost:8080/orderItems/orderClient/' + id,
@@ -100,11 +115,67 @@ function orderClick(id) {
                 html = html + data[tableIndex].quantity * data[tableIndex].itemMenuDet.prix;
                 html = html + " </td>"
                 html = html + "</tr>"
-                
+
             }
             //recover the html salved in the document load.
             $("#divItems_table").html(divItemsTable_html).append(html);
         }
     });
     console.log(id);
+}
+
+function addOrder(tableRoom) {
+    idTableRoom = tableRoom.idtable;
+    tableName = tableRoom.name;
+    console.log(idTableRoom + ", " + tableName);
+    var json = {
+        tableRoom: {
+            idtable: idTableRoom
+        },
+        shift: {
+            idshift: shiftId
+        },
+        openFlag: true,
+        creationDate: '1454263234000'
+    };
+    jQuery.ajax({
+        headers: {
+            'Accept': 'application/json',
+            'Content-Type': 'application/json'
+        },
+        type: 'post',
+        url: 'http://localhost:8080/orderClient/',
+        dataType: 'json',
+        data: JSON.stringify(json),
+        success: function (data, textStatus, jqXHR) {
+            var html = "";
+            tableRoomClick(tableRoom);
+        }
+    });
+}
+
+function orderRemove(order) {
+    console.log(order.idOrder);
+    /*var json = {
+        tableRoom: {
+            idtable: idTableRoom
+        },
+        shift: {
+            idshift: shiftId
+        },
+        openFlag: true,
+        creationDate: '1454263234000'
+    };*/
+    jQuery.ajax({
+        headers: {
+            'Accept': 'application/json',
+            'Content-Type': 'application/json'
+        },
+        type: 'delete',
+        url: 'http://localhost:8080/orderClient/' + order.idOrder,
+        success: function (data, textStatus, jqXHR) {
+            var html = "";
+            tableRoomClick(order.tableRoom);
+        }
+    });
 }
